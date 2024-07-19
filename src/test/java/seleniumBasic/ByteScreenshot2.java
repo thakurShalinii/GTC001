dc2d46159eba2a19898957117a2e1a792ae200ab
thakurShalinii <shalukumari726094@gmail.com>
2024-07-19 15:14:36 +0530

    My second commit

---
+++ src/test/java/seleniumBasic/ByteScreenshot2.java
@@ -0,0 +1,39 @@
+package seleniumBasic;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ByteScreenshot2 {
+
+	public static void main(String[] args) {
+		
+		 WebDriverManager.chromedriver().setup();
+	     WebDriver driver = new ChromeDriver();
+	     driver.manage().window().maximize();
+		        try {
+		        	 driver.get("https://www.instagram.com/"); 
+		            byte[] screenshotBytes = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+		            Path destination = Paths.get("C:\\Users\\shalu\\eclipse-workspace\\GTC001\\Screenshot\\img5.png");
+		            Files.write(destination, screenshotBytes);
+
+		            System.out.println("Screenshot taken and saved successfully!");
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        } finally {
+		        
+		        }
+		    }
+		
+
+
+	}
+
+
